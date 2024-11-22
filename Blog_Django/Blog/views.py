4fbdc1a6c642b421ff3e987a6b55886ff59ced7b
Juan Camilo <168372257+Jkpepo@users.noreply.github.com>
2024-11-21 22:49:13 -0500

    incorporacion de paginador
    -se agrega paginador a la vista principal
    -se inicia a trabajar en el navbar
    -se trabaja un poco en el diseño

--- Blog_Django/Blog/views.py
+++ Blog_Django/Blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render,redirect
 from . models import Publicaciones
-from . forms import PublicacionesForm
+from . forms import PublicacionesForm 
+from django.core.paginator import Paginator
 
 # Create your views here.
 def home(request):
-    publicaciones=Publicaciones.objects.all()
-    return render(request,"home.html",{"publicaciones":publicaciones})
+    lista=Publicaciones.objects.all().order_by("-id")
+    paginator = Paginator(lista, 3)
+    pagina=request.GET.get("page") or 1 #pido el page o en su defecto 1
+    publicaciones=paginator.get_page(pagina)# obtengo el numero paginas
+    pagina_actual=int(pagina)#transformo la pagina en un numero 
+    paginas=range(1,publicaciones.paginator.num_pages +1)
+    return render(request,"home.html",{"publicaciones":publicaciones,"paginas":paginas,"pagina_actual":pagina_actual})
 
 def base(request):
     return render(request,"base.html")
